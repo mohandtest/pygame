96fd90f1ba89e10074a17af0ee745faf241e4211
mohandtest <112395083+mohandtest@users.noreply.github.com>
2024-04-07 02:12:17 +0200

    Ferdigprodukt

--- Manic Mansion/karakterer.py
+++ Manic Mansion/karakterer.py
@@ -1,74 +1,124 @@
 from math import sqrt
-from pygame import draw, sprite
+from pygame import draw, sprite, Vector2, Rect, Surface
 from pygame.locals import (K_UP, K_DOWN, K_LEFT, K_RIGHT)
 
 
 class SuperKarakter(sprite.Sprite):
     """Klasse for karakter"""
-    def __init__(self, x, y, radius, farge, vindusobjekt):
+    def __init__(self, x, y, farge, vindusobjekt, bredde = 20, hoeyde = 20):
         """Konstruktør"""
         super().__init__()
         self.x = x
         self.y = y
-        self.radius = radius
         self.farge = farge
         self.vindusobjekt = vindusobjekt
+        self.bredde = bredde
+        self.hoeyde = hoeyde
+        self.image = Surface((self.bredde, self.hoeyde))
+        self.image.fill(farge)
+        self.rect = self.image.get_rect()
+        self.rect = Rect(x , y, self.bredde,self.hoeyde)
 
 
     def tegn(self):
-        """Metode for å tegne ballen"""
-        draw.circle(self.vindusobjekt, self.farge, (self.x, self.y), self.radius) 
+      draw.rect(self.vindusobjekt, self.farge, (self.rect.x, self.rect.y, self.bredde, self.hoeyde)) 
 
-    def finnAvstand(self, annetObjekt):
-        """Metode for å finne avstanden til en annen ball"""
-        xAvstand2 = (self.x - annetObjekt.x)**2  # x-avstand i andre
-        yAvstand2 = (self.y - annetObjekt.y)**2  # y-avstand i andre
-        sentrumsavstand = sqrt(xAvstand2 + yAvstand2)
-        radiuser = self.radius + annetObjekt.radius
-        avstand = sentrumsavstand - radiuser
-        return avstand  
-    
+    def sjekk_kollisjon(self, karakter):
+        """Sjekker om karakteren kolliderer med hindringen"""
+        return self.rect.colliderect(karakter.rect)
 
 class Menneske(SuperKarakter):
-    def __init__(self, x, y, fart, radius, farge, vindusobjekt):
-        super().__init__(x, y, radius, farge, vindusobjekt)
+    def __init__(self, x, y, fart, farge, vindusobjekt, poeng = 0):
+        super().__init__(x, y, farge, vindusobjekt, bredde = 20 , hoeyde= 20)
         self.fart = fart
-        
-    def tegn(self):
-        """Metode for å tegne spilleren"""
-        draw.circle(self.vindusobjekt, self.farge, (self.x, self.y), self.radius) 
+        self.poeng = poeng
+      
+    def flytt(self, taster, hindringer):
+        """Metode for å flytte mennesket"""
+        retning = Vector2(0, 0)
+        if taster[K_UP]:
+            retning.y -= 1
+        if taster[K_DOWN]:
+            retning.y += 1
+        if taster[K_LEFT]:
+            retning.x -= 1
+        if taster[K_RIGHT]:
+            retning.x += 1
 
-    def flytt(self, taster):
-      """Metode for å flytte ballen"""
-      if taster[K_UP]:
-        self.y -= self.fart
-      if taster[K_DOWN]:
-        self.y += self.fart
-      if taster[K_LEFT]:
-        self.x -= self.fart
-      if taster[K_RIGHT]:
-        self.x += self.fart
+        # Hvis spilleren beveger seg diagonalt, normaliser retningen og deler med sqrt(2)
+        if retning.length() > 0:
+            retning.normalize_ip()
+            retning *= self.fart
+        retning = round(retning)
+        # Beregn ny posisjon
+        ny_x = self.rect.x + retning.x
+        ny_y = self.rect.y + retning.y
 
-        
+        # Opprett en rektangel for å representere den potensielle nye posisjonen
+        ny_rektangel = Rect(ny_x, ny_y, self.bredde, self.hoeyde)
+
+        # Sjekk for kollisjon med hindringer
+        kollisjon = False
+        for hindring in hindringer:
+            if hindring.sjekk_kollisjon(self):
+                kollisjon = True
+                break
+
+        # Hvis det er en kollisjon, juster den potensielle nye posisjonen
+        if kollisjon:
+            # Finn forskjellen mellom spillerens midtpunkt og hindringens midtpunkt
+            diff_x = hindring.rect.centerx - self.rect.centerx
+            diff_y = hindring.rect.centery - self.rect.centery
+            # Hvis hindringen er nærmere i x-retning
+            if abs(diff_x) > abs(diff_y):
+                # Juster den potensielle nye posisjonen i x-retning
+                ny_rektangel.x -= retning.x
+            else:
+                # Juster den potensielle nye posisjonen i y-retning
+                ny_rektangel.y -= retning.y
+
+        # Begrens bevegelsen til å holde spilleren innenfor vinduet
+        ny_rektangel.x = max(0, min(ny_rektangel.x, self.vindusobjekt.get_width() - self.bredde))
+        ny_rektangel.y = max(0, min(ny_rektangel.y, self.vindusobjekt.get_height() - self.hoeyde))
+
+        # Oppdater posisjonen
+        self.rect = ny_rektangel
+
 
 
 class Spoekelse(SuperKarakter):
-  """Klasse for å representere et hinder"""
-  def __init__(self, x, y, radius, farge, vindusobjekt, xFart, yFart):
-    super().__init__(x, y, radius, farge, vindusobjekt)
-    self.xFart = xFart
-    self.yFart = yFart
+    """Klasse for å representere et hinder"""
 
-  def flytt(self):
-    """Metode for å flytte spøkelset"""
-    # Sjekker om hinderet er utenfor høyre/venstre kant
-    if ((self.x - self.radius) <= 0) or ((self.x + self.radius) >= self.vindusobjekt.get_width()):
-      self.xFart = -self.xFart
+    def __init__(self, x, y, farge, vindusobjekt, xFart, yFart, sone):
+        super().__init__(x, y, farge, vindusobjekt, bredde=20, hoeyde=20)
+        self.xFart = xFart
+        self.yFart = yFart
+        self.sone = sone
+        
+
+    def flytt(self):
+        """Metode for å flytte spøkelset"""
+        # Sjekk om hinderet er i midtsonen
+        if self.sone.colliderect(self.rect):
+            # Sjekker om hinderet er utenfor venstre/høyre kant
+            if ((self.x - self.bredde) <= self.sone.left) or ((self.x + self.bredde) >= self.sone.right):
+                self.xFart = -self.xFart
+
+            # Sjekker om hinderet er utenfor øvre/nedre kant
+            if ((self.y - self.hoeyde) <= self.sone.top) or ((self.y + self.hoeyde) >= self.sone.bottom):
+                self.yFart = -self.yFart
+
+        # Flytter hinderet
+        self.x += self.xFart
+        self.y += self.yFart
+        self.rect.centerx = self.x
+        self.rect.centery = self.y
+
+class Hindring(SuperKarakter):
+    def __init__(self, x, y, farge, vindusobjekt, bredde=20, hoeyde=20):
+        super().__init__(x, y, farge, vindusobjekt, bredde, hoeyde)
+
     
-    # Sjekker om hinderet er utenfor øvre/nedre kant
-    if ((self.y - self.radius) <= 0) or ((self.y + self.radius) >= self.vindusobjekt.get_height()):
-      self.yFart = -self.yFart
-
-    # Flytter hinderet
-    self.x += self.xFart
-    self.y += self.yFart
+class Sau(SuperKarakter):
+    def __init__(self, x, y, farge, vindusobjekt, bredde=20, hoeyde=20):
+        super().__init__(x, y, farge, vindusobjekt, bredde, hoeyde)
--- Manic Mansion/main.py
+++ Manic Mansion/main.py
@@ -1,45 +1,130 @@
 import pygame
 from konstanter import *
-from karakterer import Menneske
+from karakterer import *
+import random
 
 # For å gi visuelt inntrykk av sonene. Har delt brettet i 6 deler, der midtsonen er 4/6 stor
 menneske_sone = pygame.Rect(0, 0, BREDDE//6, HOEYDE)
 midtsone = pygame.Rect(BREDDE//6, 0, 2*BREDDE//3, HOEYDE)
 saue_sone = pygame.Rect(5*BREDDE//6, 0, 1*BREDDE//6, HOEYDE)
 
-# Initialiserer pygame lager vinduet
+def vis_poeng(vindu, poeng):
+    font = pygame.font.SysFont(None, 36)  # Velger font og størrelse
+    tekst = font.render(f"Poeng: {poeng}", True, SVART)  # Lager tekstobjekt
+    vindu.blit(tekst, (BREDDE - tekst.get_width() - 10, 10))  # Tegner tekstobjektet på skjermen
+
+
 pygame.init()
-vindu = pygame.display.set_mode((BREDDE, HOEYDE))
-pygame.display.set_caption("Manic Mansion")
-clock = pygame.time.Clock()     # For å sette FPS
 
-# Oppretter spillebrettet
-spillebrett = pygame.sprite.Group()
+def main():
 
-menneske = Menneske(BREDDE/6, HOEYDE/2, 4, 15,BLAA,vindu)
+    # Initialiserer pygame lager vinduet
+    vindu = pygame.display.set_mode((BREDDE, HOEYDE))
+    pygame.display.set_caption("Manic Mansion")
+    clock = pygame.time.Clock()     # For å sette FPS
 
-# Spill løkka
-fortsett = True
-while fortsett:
+    # Oppretter spoekelseret
+    spoekelser = pygame.sprite.Group()
+    sauer = pygame.sprite.Group()
+    hindringer = []
+    holdt_sau = None
 
-    #1 Prosseserer pygame input
-    for event in pygame.event.get():        # Nødvendig for pygame
-        # Ser etter om pygame skal avslutte
-        if event.type == pygame.QUIT:
-            fortsett = False
- 
+    # Karakterer
+    menneske = Menneske((BREDDE/6)/2, HOEYDE/2, STARTSFART, BLAA, vindu)
+    starts_antall = 3
 
-    trykkede_taster = pygame.key.get_pressed()
-    menneske.flytt(trykkede_taster)
+    for _ in range(starts_antall):
+        # Generer tilfeldige koordinater innenfor midtsonen
+        x = random.randint(midtsone.left + 30, midtsone.right - 30)
+        y = random.randint(midtsone.top + 30, midtsone.bottom - 30)
+        # Oppretter spøkelse
+        spokelse = Spoekelse(x, y, ROED, vindu, 5, 5, midtsone)
+        spoekelser.add(spokelse)
+        hindring = Hindring(x,y,SVART,vindu)
+        hindringer.append(hindring)
+        ny_sau = Sau(random.randint(saue_sone.left + 20, saue_sone.right - 20), random.randint(saue_sone.top + 20, saue_sone.bottom - 20), HVIT, vindu)
+        sauer.add(ny_sau)
 
-    #3 Tegner karakterer og bakgrunnen
-    vindu.fill(SVART)
-    pygame.draw.rect(vindu, (100, 100, 100), menneske_sone)
-    pygame.draw.rect(vindu, (200, 200, 200), midtsone)
-    pygame.draw.rect(vindu, (100, 100, 100), saue_sone)
-    menneske.tegn()
+    
 
+    # Spill løkka
+    fortsett = True
+    while fortsett:
 
-    clock.tick(FPS)     # Gjør at loopen tikker etter FPS - Stabilitet
-    pygame.display.flip()  
-pygame.quit()
+        # Prossesere pygame input
+        for event in pygame.event.get():        # Nødvendig for pygame
+            # Ser etter om pygame skal avslutte
+            if event.type == pygame.QUIT:
+                fortsett = False
+    
+        # Bevegelsesrelatert funksjonalitet
+        trykkede_taster = pygame.key.get_pressed()
+        menneske.flytt(trykkede_taster,hindringer)
+        for spokelse in spoekelser:
+            spokelse.flytt()
+        
+        # Kollisjonsrelatert funksjonalitet
+
+        # Kollisjon med spøkelse:
+        kolliderende_spoekelser = pygame.sprite.spritecollide(menneske, spoekelser, False)
+        if kolliderende_spoekelser:
+            # Mennesket kolliderer med et spøkelse
+            print("Mennesket har kollidert med et spøkelse!")
+            fortsett = False  # Avslutt spillet
+
+        # Kollisjon med sau:
+        kolliderende_sauer = pygame.sprite.spritecollide(menneske, sauer, True)
+        for sau in kolliderende_sauer:
+            if holdt_sau is None:
+                holdt_sau = sau
+                menneske.fart -= FARTSFAKTOR  # Reduserer spillerens fart når han holder på en sau
+                menneske.farge = GROENN
+            else:
+                # Spillet avsluttes hvis spilleren allerede holder på en sau og treffer en annen sau
+                fortsett = False
+
+        # Sjekker om mennesket har kommet tilbake til menneskesonen med en sau
+        if menneske.rect.colliderect(menneske_sone):
+            if holdt_sau is not None:
+                # Øker poeng og fjerner sau
+                menneske.poeng += 1
+                sauer.remove(holdt_sau)
+                holdt_sau = None
+                menneske.fart += FARTSFAKTOR  # Setter spillerens fart tilbake til normalt
+                menneske.farge = BLAA
+
+                # Legger til ny sau
+                ny_sau = Sau(random.randint(saue_sone.left + 20, saue_sone.right - 20),
+                             random.randint(saue_sone.top + 20, saue_sone.bottom - 20),
+                             HVIT, vindu)
+                sauer.add(ny_sau)
+
+                # Legger til nytt spøkelse og hindring
+                x = random.randint(midtsone.left + 20, midtsone.right - 20)
+                y = random.randint(midtsone.top + 20, midtsone.bottom - 20)
+                nytt_spokelse = Spoekelse(x, y ,ROED, vindu, 5, 5, midtsone)
+                spoekelser.add(nytt_spokelse)
+                hindring = Hindring(x,y,SVART,vindu)
+                hindringer.append(hindring)
+
+        
+        # Tegner karakterer og bakgrunnen
+        vindu.fill(SVART)
+        
+        pygame.draw.rect(vindu, (100, 100, 100), menneske_sone)
+        pygame.draw.rect(vindu, (200, 200, 200), midtsone)
+        pygame.draw.rect(vindu, (100, 100, 100), saue_sone)
+
+        menneske.tegn()
+        spoekelser.draw(vindu)
+        sauer.draw(vindu)
+        for hindring in hindringer:
+            hindring.tegn()
+
+        vis_poeng(vindu,menneske.poeng)
+        clock.tick(FPS)     # Gjør at loopen tikker etter FPS - Stabilitet
+        pygame.display.flip()  
+
+if __name__ == '__main__':
+    main()
+    pygame.quit()
